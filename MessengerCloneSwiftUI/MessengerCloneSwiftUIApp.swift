398156c0ed179a7e864ce462cf0848c69566895a
Sergey Petrosyan <ipetriez@icloud.com>
2024-01-02 13:24:07 +0400

    Initial Commit

---
+++ MessengerCloneSwiftUI/MessengerCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneSwiftUIApp.swift
+//  MessengerCloneSwiftUI
+//
+//  Created by Sergey Petrosyan on 02.01.24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
